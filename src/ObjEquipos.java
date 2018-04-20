00de1777067be1441cf5e1b7fcedb5201e357fb3
Killian Herrera Garcia <41011561p@iespoblenou.org>
2018-04-20 15:38:18 +0200

    ObjEquipos actualizado

--- src/ObjEquipos.java
+++ src/ObjEquipos.java
@@ -3,27 +3,31 @@ import java.util.*;
 public class ObjEquipos {
 
     private String nomEquip;
-    private int victorias;
-    private int empates;
-    private int derrotas;
-    private int puntos;
     private ArrayList<Jugadores> jugadores = new ArrayList<Jugadores>();
+    private int victorias;
+    private int derrotas;
+    private int empates;
+    private int puntos;
 
     @Override
     public String toString() {
         return "ObjEquipos{" +
                 "nomEquip='" + nomEquip + '\'' +
                 ", jugadores=" + jugadores +
+                ", victorias=" + victorias +
+                ", derrotas=" + derrotas +
+                ", empates=" + empates +
+                ", puntos=" + puntos +
                 '}';
     }
 
-    public ObjEquipos(String nomEquip,ArrayList<Jugadores> jugadores) {
+    public ObjEquipos(String nomEquip, ArrayList<Jugadores> jugadores) {
         this.nomEquip = nomEquip;
-        this.victorias = 0;
-        this.empates = 0;
-        this.derrotas = 0;
-        this.puntos = 0;
         this.jugadores = jugadores;
+        this.victorias = 0;
+        this.derrotas = 0;
+        this.empates = 0;
+        this.puntos = 0;
     }
 
     public String getNomEquip() {
@@ -38,9 +42,43 @@ public class ObjEquipos {
         this.nomEquip = nomEquip;
     }
 
+    public void setVictorias(int victorias) {
+        this.victorias = victorias;
+    }
+
+    public void setDerrotas(int derrotas) {
+        this.derrotas = derrotas;
+    }
+
+    public void setEmpates(int empates) {
+        this.empates = empates;
+    }
+
+    public void setPuntos(int puntos) {
+        this.puntos = puntos;
+    }
+
+    public int getVictorias() {
+
+        return victorias;
+    }
+
+    public int getDerrotas() {
+        return derrotas;
+    }
+
+    public int getEmpates() {
+        return empates;
+    }
+
+    public int getPuntos() {
+        return puntos;
+    }
+
     public void setJugadores(ArrayList<Jugadores> jugadores) {
 
         this.jugadores = jugadores;
+
     }
 
 }
